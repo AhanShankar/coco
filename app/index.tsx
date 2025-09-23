ffb1a837bdc9de6a8db1cd96a6d804b6b4cd1dd4
AhanShankar <ahanshankar@gmail.com>
2025-09-23 18:05:30 +0530

    remove boilerplate

---
+++ app/index.tsx
@@ -0,0 +1,10 @@
+// app/index.tsx
+import { Text, View } from 'react-native';
+
+export default function HomeScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', }}>
+      <Text style={{ fontSize: 24, fontWeight: 'bold' }}>Welcome to Coco App 🎉</Text>
+    </View>
+  );
+}
