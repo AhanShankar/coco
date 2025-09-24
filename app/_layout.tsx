03e345307b31861c5a4e9ca32aff2fe4f580de74
AhanShankar <ahanshankar@gmail.com>
2025-09-24 10:57:31 +0530

    change orientation to landscape using expo-screen-orientation

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,13 +3,18 @@ import 'react-native-reanimated';
 
 import { useColorScheme } from '@/hooks/use-color-scheme';
 import { Stack } from 'expo-router';
+import * as ScreenOrientation from 'expo-screen-orientation';
 
+
+async function changeScreenOrientation(orientation: ScreenOrientation.OrientationLock) {
+  await ScreenOrientation.lockAsync(orientation);
+}
 export default function RootLayout() {
   const colorScheme = useColorScheme();
-
+  changeScreenOrientation(ScreenOrientation.OrientationLock.LANDSCAPE);
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack></Stack>
+      <Stack screenOptions={{ headerShown: false }}></Stack>
     </ThemeProvider>
   );
 }
