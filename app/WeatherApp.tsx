f54ba82530c641b44ff3af8c6ecc83e738f20938
AhanShankar <ahanshankar@gmail.com>
2025-09-26 17:15:36 +0530

    fix: temp comment navbar useeffect

--- app/WeatherApp.tsx
+++ app/WeatherApp.tsx
@@ -60,24 +60,24 @@ export default function WeatherApp() {
       if (intervalId) clearInterval(intervalId);
     };
   }, []);
-  useEffect(() => {
-    if (Platform.OS === 'android') {
-      const setImmersive = async () => {
-        await NavigationBar.setBehaviorAsync('overlay-swipe');
-        await NavigationBar.setVisibilityAsync('hidden');
-      };
-      setImmersive();
-      const subscription = NavigationBar.addVisibilityListener(({ visibility }) => {
-        if (visibility === 'visible') {
-          setImmersive();
-        }
-      });
+  // useEffect(() => {
+  //   if (Platform.OS === 'android') {
+  //     const setImmersive = async () => {
+  //       await NavigationBar.setBehaviorAsync('overlay-swipe');
+  //       await NavigationBar.setVisibilityAsync('hidden');
+  //     };
+  //     setImmersive();
+  //     const subscription = NavigationBar.addVisibilityListener(({ visibility }) => {
+  //       if (visibility === 'visible') {
+  //         setImmersive();
+  //       }
+  //     });
 
-      return () => {
-        subscription.remove();
-      };
-    }
-  }, []);
+  //     return () => {
+  //       subscription.remove();
+  //     };
+  //   }
+  // }, []);
   if (loading) {
     return (
       <View style={styles.container}>
